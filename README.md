2d51de90c982c2af6c27d1becbb0a501e0599e17
Muteb <muteb.faleh@gmail.com>
2018-11-05 00:34:42 +0300

    PEP 8 -- Style Guide for Python Code

--- README.md
+++ README.md
@@ -29,6 +29,29 @@ cd /vagrant to change to your vagrant directory.
 psql -d news -f newsdata.sql to load the data and create the tables.
 python3 newsdata.py to run the reporting tool.
 
+These queries are complicated. Where do I start?
+One of the best ways to build complex queries is by starting with smaller pieces, and testing each of them in small steps. Here's a worked example —
+
+Suppose we wanted to print out each article's title and author name.
+
+Looking at the schema for articles (with \d articles) we can see there's an author and title column. But the author column doesn't have names in it — just numbers. To see this in your database, run:
+
+select author from articles;
+But the authors table has a name column, and a numeric id column. To see this, run:
+
+select * from authors;
+Those numeric id values match up with the articles.author column. And that means we can connect the two tables with a join:
+
+select title, name
+from articles join authors
+on articles.author = authors.id;
+or:
+
+select title, name
+from articles, authors
+where articles.author = authors.id;
+Try these queries on your news database! Look for other relationships that can work with join.
+
 
 
 
--- log.report.py
+++ log.report.py
@@ -2,28 +2,29 @@
 
 import psycopg2
 
-topArticles="""select articles.title, count(*) 
-    as countNum from log, articles where 
+topArticles = """select articles.title, count(*)
+    as countNum from log, articles where
     log.status='200 OK' and articles.slug=
     substr(log.path, 10) group by articles.title
     order by countNum desc limit 3;"""
 
-popularAuthors="""select authors.name, 
+popularAuthors = """select authors.name,
     count(*) as countNum from articles,
     authors, log where log.status='200 OK'
-    and authors.id = articles.author and 
+    and authors.id = articles.author and
     articles.slug = substr(log.path, 10)
-    group by authors.name order by 
+    group by authors.name order by
     countNum desc limit 3;"""
 
-requestsErrors="""select date1, reqsts_sent, err1,100.0 
+requestsErrors = """select date1, reqsts_sent, err1,100.0
     * err1 / reqsts_sent as pert from (select date_trunc
-    ('day', time) as reqdate, count(*) as reqsts_sent from 
+    ('day', time) as reqdate, count(*) as reqsts_sent from
     log group by reqdate)as requests,(select date_trunc
-    ('day', time) as date1,count(*)as err1 from log where 
-    status = '404 NOT FOUND'group by date1)as errors where 
-    reqdate = date1 and errors.err1 > 0.01 * 
+    ('day', time) as date1,count(*)as err1 from log where
+    status = '404 NOT FOUND'group by date1)as errors where
+    reqdate = date1 and errors.err1 > 0.01 *
     requests.reqsts_sent order by date1 desc;"""
+
 
 def dbNws(query):
     conn = psycopg2.connect(database="news")
@@ -33,8 +34,10 @@ def dbNws(query):
     conn.close()
     return results
 
+
 def the_title(title):
-    print ("\n" + title + "\n")
+    print("\n" + title + "\n")
+
 
 def topmost_articles():
     topmost_articles = dbNws(topArticles)
@@ -42,18 +45,21 @@ def topmost_articles():
     for title, num in topmost_articles:
         print(" |{}| --> {} views".format(title, num))
 
+
 def topmost_authors():
     topmost_authors = dbNws(popularAuthors)
     the_title("(Q2)--> The most popular article authors of all time.")
     for name, num in topmost_authors:
         print(" |{}        | --> {} views".format(name, num))
 
+
 def errors():
     errors = dbNws(requestsErrors)
     # Print results
-    the_title ("(Q3)-->Days more than 1% of requests lead to errors.")
+    the_title("(Q3)-->Days more than 1% of requests lead to errors.")
     for date1, reqsts_sent, err1, pert in errors:
         print("\t{:} {:.3f}% \n\tGood".format(date1, pert))
+
 
 if __name__ == '__main__':
     topmost_articles()
